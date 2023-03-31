74454d30e5482f8f1b743340817004e37e72cd4f
William Kerby <w.kerby99@gmail.com>
2023-03-31 00:28:32 -0400

    add to get all products endpoint

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,9 +4,22 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  try {
+      // find all products
+    const prodData = await Product.findAll({
+      // be sure to include its associated Category and Tag data
+      include: [{ model : Category}, { model : Tag }]
+    }); 
+
+    res.status(200).json(prodData);
+
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
+  
+
 });
 
 // get one product
