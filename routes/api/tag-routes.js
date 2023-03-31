b8a8da790dba7faa4691ec5cca08b0bd5fe64b04
William Kerby <w.kerby99@gmail.com>
2023-03-31 00:47:40 -0400

    add to get all tags and get tag by id endpoints

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,14 +3,43 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+router.get('/', async (req, res) => {
+  try {
+     // find all tags
+  const tagData = await Tag.findAll({
+    // be sure to include its associated Product data
+    include: [{ model : Product}]
+  }); 
+
+  res.status(200).json(tagData);
+
+}
+  catch (err) {
+  res.status(500).json(err);
+}
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+router.get('/:id', async (req, res) => {
+  try {
+    // find a single tag by its `id`
+  const tagData = await Tag.findByPk(req.params.id, {
+    // be sure to include its associated Product data
+    include: [{ model : Product}]
+  });
+
+  if (!tagData) {
+    res.status(404).json({ message: 'Oops! No product found with that id!'});
+    return;
+  }
+
+  res.status(200).json(tagData);
+
+}
+catch (err) {
+  res.status(500).json(err);
+
+}
+
 });
 
 router.post('/', (req, res) => {
